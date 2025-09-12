50c005c668b05919afb6fa7b4219c9765af07881
Erik de Sousa Gonçalves <erikdesousagoncalves1009@gmail.com>
2025-09-12 11:39:06 -0300

    fetch(PageGame.jsx) implementando pagina individual dos games, sem review

--- gameTracker-Backend/src/domain/games/repository/gamesRepository.js
+++ gameTracker-Backend/src/domain/games/repository/gamesRepository.js
@@ -10,7 +10,18 @@ class GamesRepository{
     }
 
     async findById(id){
-      const [row]=await db.query("SELECT * FROM games where id =?",[id])
+      const [row]=await db.query(`
+      SELECT g.*,
+             GROUP_CONCAT(DISTINCT p.nome) AS plataformas,
+             GROUP_CONCAT(DISTINCT ge.nome) AS generos
+      FROM games g
+      LEFT JOIN game_plataformas gp ON g.id = gp.id_game
+      LEFT JOIN plataformas p ON gp.id_plataforma = p.id
+      LEFT JOIN game_generos gg ON g.id = gg.id_game
+      LEFT JOIN generos ge ON gg.id_genero = ge.id
+      WHERE g.id = ?
+      GROUP BY g.id
+    `,[id])
       return row[0]
     }
 
