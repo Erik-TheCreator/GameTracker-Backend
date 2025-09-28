926356bc1c9cc4a38f5cacccbd1d946657853a3e
Erik-TheCreator <erikdesousagoncalves1009@gmail.com>
2025-09-28 14:12:44 -0300

    fetch(userRoute.jsx) função de deslogar

--- gameTracker-Backend/src/domain/users/routes/userRoute.js
+++ gameTracker-Backend/src/domain/users/routes/userRoute.js
@@ -6,6 +6,11 @@ router.get("/me", userController.getUser);
 router.post("/", userController.cadastrar);
 router.put("/", userController.updatePerfil);
 router.get("/usuario/:id", userController.getUserById);
+router.post("/logout", (req, res) => {
+  res.clearCookie("token"); 
+  return res.status(200).json({ mensagem: "Logout realizado com sucesso" });
+});
+
 
 
 module.exports = router;
